4b785a4399b1525b37655a8766d999ec77b897fa
Chris Zimmerman <chris-zimmerman@live.com>
2019-11-19 09:54:43 -0600

    Stop checking in bad stuff

--- src/bottle.cr
+++ src/bottle.cr
@@ -4,28 +4,3 @@ module Bottle
   extend self
   VERSION = "0.2.2"
 end
-
-include Bottle
-
-a = B.arange(12).reshape([3, 4])
-b = a  # no copy of the tensors data is made
-
-c = a.dup_view()
-
-puts a.buffer == c.buffer
-puts c.flags.own_data?
-
-c = c.reshape([2, 6])
-c[[0, 4]] = 12345
-puts a
-
-s = a[..., 1...3]
-s[...] = 10
-
-puts a
-
-d = a.dup
-puts d.buffer == a.buffer
-
-d[[0, 0]] = 9999
-puts a
