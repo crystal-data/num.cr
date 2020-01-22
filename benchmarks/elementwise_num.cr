b2c755a9d257b16318377dc1c7f56ccc710f652b
Chris Zimmerman <chris-zimmerman@live.com>
2020-01-22 11:31:39 -0600

    Fixed ameba and hopefully deploy docs

--- benchmarks/elementwise_num.cr
+++ benchmarks/elementwise_num.cr
@@ -2,10 +2,10 @@ require "../src/num"
 
 def elementwise(n : Int32)
   puts "\nNum.cr elementwise #{n}  1.0 + i / 1.0 + j * k"
-  i = Tensor.random(0.0...1.0, [n])
-  j = Tensor.random(0.0...1.0, [n])
-  k = Tensor.random(0.0...1.0, [n])
-  i.map3(j, k) { |i, j, k| 1.0 + i / 1.0 + j * k }
+  x = Tensor.random(0.0...1.0, [n])
+  y = Tensor.random(0.0...1.0, [n])
+  z = Tensor.random(0.0...1.0, [n])
+  x.map3(y, z) { |i, j, k| 1.0 + i / 1.0 + j * k }
 end
 
 n = (ARGV[0]? || 100).to_i
