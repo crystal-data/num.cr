6514eee87e8f2b887341bb2f095140f28e03e7f0
Chris Zimmerman <chris-zimmerman@live.com>
2020-10-05 16:20:18 -0400

    outer operators

--- src/tensor/internal/yield_iterators.cr
+++ src/tensor/internal/yield_iterators.cr
@@ -135,3 +135,62 @@ def tri_strided_iteration(t1 : Tensor, t2 : Tensor, t3 : Tensor)
     end
   end
 end
+
+@[AlwaysInline]
+def outer_strided_iteration(t1 : Tensor, t2 : Tensor)
+  n = t1.size
+  m = t2.size
+
+  index = 0
+
+  t1_contiguous = t1.flags.contiguous?
+  t2_contiguous = t2.flags.contiguous?
+
+  t1data = t1.to_unsafe
+  t2data = t2.to_unsafe
+
+  t1_shape, t1_strides, t1_rank = t1.iter_attrs
+  t2_shape, t2_strides, t2_rank = t2.iter_attrs
+
+  if t1_contiguous && t2_contiguous
+    n.times do
+      m.times do
+        yield index, t1data, t2data
+        t2data += 1
+        index += 1
+      end
+      t1data += 1
+    end
+  elsif t1_contiguous
+    init_strided_iteration(:t2_coord, :t2_backstrides, t2_shape, t2_strides, t2_rank)
+    n.times do
+      m.times do
+        yield index, t1data, t2data
+        advance_strided_iteration(:t2_coord, :t2_backstrides, t2_shape, t2_strides, t2_rank, t2data)
+        index += 1
+      end
+      t1data += 1
+    end
+  elsif t2_contiguous
+    init_strided_iteration(:t1_coord, :t1_backstrides, t1_shape, t1_strides, t1_rank)
+    n.times do
+      m.times do
+        yield index, t1data, t2data
+        index += 1
+        t2data += 1
+      end
+      advance_strided_iteration(:t1_coord, :t1_backstrides, t1_shape, t1_strides, t1_rank, t1data)
+    end
+  else
+    init_strided_iteration(:t1_coord, :t1_backstrides, t1_shape, t1_strides, t1_rank)
+    init_strided_iteration(:t2_coord, :t2_backstrides, t2_shape, t2_strides, t2_rank)
+    n.times do
+      m.times do
+        yield index, t1data, t2data
+        index += 1
+        advance_strided_iteration(:t2_coord, :t2_backstrides, t2_shape, t2_strides, t2_rank, t2data)
+      end
+      advance_strided_iteration(:t1_coord, :t1_backstrides, t1_shape, t1_strides, t1_rank, t1data)
+    end
+  end
+end
